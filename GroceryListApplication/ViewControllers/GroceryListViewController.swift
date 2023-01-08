adf88233e7ee1ea5e8f524881207a1a994f45c4a
alsiraji <m.a.alseraji@gmail.com>
2023-01-08 10:24:45 +0000

    Firebase SDK and initial design

---
+++ GroceryListApplication/ViewControllers/GroceryListViewController.swift
@@ -0,0 +1,18 @@
+//
+//  GroceryListViewController.swift
+//  GroceryListApplication
+//
+//  Created by Mohammed Alsiraji on 08/01/2023.
+//
+
+import UIKit
+
+class GroceryListViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+  
+    }
+  
+}
